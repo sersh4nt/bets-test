6cc394ab5b743943bce6e7ef2cbefcf28fb4730e
sersh4nt <rektf12@gmail.com>
2022-07-14 16:37:31 +0400

    fix docker config so it builds

--- api/core/asgi.py
+++ api/core/asgi.py
@@ -5,11 +5,10 @@ from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
 
-from api.routing import websocket_urlpatterns
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
-
 django_asgi_app = get_asgi_application()
+
+from api.routing import websocket_urlpatterns
 
 application = ProtocolTypeRouter(
     {
